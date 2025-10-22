4f34d0e74dc07c6ba98567e3d789aa95ba20d2b3
Tonizhan325 <angelzhan666@gmail.com>
2025-10-22 18:15:18 +0200

    Test implementados

--- TestActividadTDDApp/FuncionesTest.cs
+++ TestActividadTDDApp/FuncionesTest.cs
@@ -1,8 +1,27 @@
-﻿namespace TestActividadTDDApp
+﻿using ActividadTDDApp;
+
+namespace TestActividadTDDApp
 {
     [TestClass]
     public sealed class FuncionesTest
     {
-        
+        [TestMethod]
+        public long Es_Menor_A_0()
+        {
+            long result = Funciones.CalcularFactorial(-2);
+            Assert.AreEqual(-1, result);
+        }
+
+        public long Es_Igual_A_0()
+        {
+            long result = Funciones.CalcularFactorial(0);
+            Assert.AreEqual(1, result);
+        }
+
+        public long Es_Mayor_A_0()
+        {
+            long result = Funciones.CalcularFactorial(5);
+            Assert.AreEqual(120, result);
+        }
     }
 }
